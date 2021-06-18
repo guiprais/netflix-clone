8ac336ade3780907456b0fe1158f5f7dbd0220fa
Guilherme de Prais <guilherme.prais@gmail.com>
2021-06-18 00:12:48 -0300

    second part

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 function App() {
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
       const list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      const originals = list.filter(({ slug }) => slug === 'originals');
+      const randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      const chosen = originals[0].items.results[randomChosen];
+      const chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
     };
 
     loadAll();
@@ -17,6 +25,9 @@ function App() {
 
   return (
     <div className="page">
+      {featuredData
+      && <FeaturedMovie item={featuredData} />}
+
       <section className="lists">
         {movieList.map(({ title, items }) => (
           <MovieRow key={title} title={title} items={items} />
--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './MovieRow.css';
+import PropTypes from 'prop-types';
 
 const MovieRow = ({ title, items }) => (
-  <div>
+  <div className="movieRow">
     <h2>{title}</h2>
     <div className="movieRow--listarea">
-      {items.results.length > 0 && items.results
-        .map(({ original_title: originalTitle, poster_path: posterPath }) => (
-          <img src={`https://image.tmdb.org/t/p/w300${posterPath}`} alt={originalTitle} />
-        ))}
+      <div className="movieRow--list">
+        {items.results.length > 0 && items.results
+          .map(({ original_title: originalTitle, poster_path: posterPath }) => (
+            <div key={originalTitle} className="movieRow--item">
+              <img src={`https://image.tmdb.org/t/p/w300${posterPath}`} alt={originalTitle} />
+            </div>
+          ))}
+      </div>
+
     </div>
   </div>
 );
