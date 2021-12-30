2cd0583227204ff227d135bb2a59edb38e2e89a6
Guilherme de Prais <guilherme.prais@gmail.com>
2021-12-30 18:36:19 -0300

    feat: movie row buttons

--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import './MovieRow.css';
 import PropTypes from 'prop-types';
 
+import { NavigateBefore, NavigateNext } from '@material-ui/icons';
+
 const MovieRow = ({ title, items }) => (
   <div className="movieRow">
     <h2>{title}</h2>
+
+    <div className="movieRow--left">
+      <NavigateBefore style={{ fontSize: 50 }} />
+    </div>
+
+    <div className="movieRow--right">
+      <NavigateNext style={{ fontSize: 50 }} />
+    </div>
+
     <div className="movieRow--listarea">
       <div className="movieRow--list">
         {items.results.length > 0 && items.results
